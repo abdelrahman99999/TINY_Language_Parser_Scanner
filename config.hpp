8b8c1b9aed7f21065ff104b7fca2d4d66cd39850
Abdelrahman Elsayed Mohamed <60861577+abdelrahman99999@users.noreply.github.com>
2022-11-30 11:27:17 +0200

    Compiler phase 1 files

---
+++ config.hpp
@@ -0,0 +1,9 @@
+#ifndef  _CONFIG_H_
+#define _CONFIG_H_
+
+
+#define DEBUG_ENABLE           0x01
+#define DEBUG_DISABLE          0x00
+#define DEBUG_INFO_ENABLE      DEBUG_DISABLE
+
+#endif /*_CONFIG_H_*/
