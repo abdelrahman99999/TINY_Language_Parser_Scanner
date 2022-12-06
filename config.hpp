3511b7ae44721ccc8bf34bcb0efdb580828d6fe3
Abdelrahman Elsayed Mohamed <60861577+abdelrahman99999@users.noreply.github.com>
2022-12-06 14:09:23 +0200

    restruction of project file

--- config.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef  _CONFIG_H_
-#define _CONFIG_H_
-
-
-#define DEBUG_ENABLE           0x01
-#define DEBUG_DISABLE          0x00
-#define DEBUG_INFO_ENABLE      DEBUG_DISABLE
-
-#endif /*_CONFIG_H_*/
